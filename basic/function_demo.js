4db373f4c5737d3e00492502ffa031f33f9963b9
Sani Kamal <sanikamal223@gmail.com>
2019-10-31 11:29:50 +0530

    add function demo

---
+++ basic/function_demo.js
@@ -0,0 +1,17 @@
+//1 A Functions which prints Hello World
+
+function printHelloWorld() {
+	return "Hello World";
+}
+console.log( printHelloWorld() );
+alert( printHelloWorld() );
+document.write( printHelloWorld() );
+
+
+//2 A Function which used to add two numbers.
+
+function add (num1, num2) {
+    var total = num1 + num2;
+    return total;		
+}
+document.write(add(45, 100));
