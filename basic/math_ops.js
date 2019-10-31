bea2550ebfe18ca4175395696470eb666ba00f54
Sani Kamal <sanikamal223@gmail.com>
2019-10-31 11:23:13 +0530

    add operators example

---
+++ basic/math_ops.js
@@ -0,0 +1,21 @@
+var apple = 45;
+var orange = 30;
+var banana = 50;
+var totalOfFruits = apple / orange + banana;
+document.write(totalOfFruits);
+
+var mod = 2018 % 2;
+document.write(mod);
+
+
+var num1 = "1345.12";
+var num2 = "42340.23";
+var totalNums = parseFloat(num1) + parseFloat(num2);
+document.write(totalNums);
+
+
+var num1 = prompt("Enter First Number 1/2");
+var num2 = prompt("Enter Last Number 2/2");
+var totalNumbers = parseInt(num1) + parseInt(num2);
+document.write(totalNumbers);
+alert(totalNumbers);
