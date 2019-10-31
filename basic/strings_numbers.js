038cf99415d7f63548360e1a9ad1bf9d49fe4d5f
Sani Kamal <sanikamal223@gmail.com>
2019-10-31 11:25:32 +0530

    add string and number example

---
+++ basic/strings_numbers.js
@@ -0,0 +1,23 @@
+//String Functions.
+
+var sentence = "Hello World";
+var stringLegnth = sentence.length;
+var Stringindex = sentence.indexOf('d');
+var subString = sentence.substring(0, 5);
+var replaceString = sentence.replace('Hello','Greetings');
+document.write(stringLegnth); // You can check each line
+
+
+//Using variable methods.
+
+var age = 19;
+var birthday = 1;
+age = age.toString();
+var age = age + birthday;
+document.write(age);
+
+
+//Example with float.
+var price = 2500.5544;
+price = price.toFixed(1);
+document.write(price);
