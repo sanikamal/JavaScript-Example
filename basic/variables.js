033575eb1dc6cc1b1d8efffc96100eabc4f841c1
Sani Kamal <sanikamal223@gmail.com>
2019-10-31 11:19:46 +0530

    add variable example

---
+++ basic/variables.js
@@ -0,0 +1,23 @@
+//Variable Declaration
+
+var firstName; // This is first name var
+var lastName; // This is last name var
+
+
+//Assigning Values to Variables.
+
+firstName = "Sani"; // String
+lastName = "Kamal"; // String
+age = 19; // Integer
+marks = 55.55; // Float
+study = false; // Boolean
+
+
+
+//Checking the Output
+
+console.log(firstName + " " + lastName)
+document.write(firstName + " " + lastName);
+alert(firstName + " " + lastName);
+
+
