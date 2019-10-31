6d84a40e7466ff15ec14ef3d733c5d9582691498
Sani Kamal <sanikamal223@gmail.com>
2019-10-31 11:31:33 +0530

    array example

---
+++ basic/array_demo.js
@@ -0,0 +1,15 @@
+var books = ['Physics','Python','Maths'];
+ 
+books[3] = "Java";
+ 
+document.write(books[3]);
+ 
+delete books[0];
+ 
+books.length
+books.sort();
+books.reverse();
+books.push('abc');
+books.unshift('abc');
+books.shift();
+books.pop();
